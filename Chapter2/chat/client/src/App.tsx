ec609aca1397c5db054791ee27160a3898cfe22c
RickCZG <czg18366118377@yahoo.com>
2019-11-29 10:15:41 +0800

    第二章：chat

--- Chapter2/chat/client/src/App.tsx
+++ Chapter2/chat/client/src/App.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import './App.css';
+import io from 'socket.io-client';
 
-const socket = new WebSocket('ws://127.0.0.1:3000');
+const socket = io('http://localhost:3000');
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Client
-      </header>
-    </div>
-  );
+class App extends React.Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    }
+  }
+
+  handleMessageInput(e) {
+    this.setState(
+      {message: e.target.value}
+    );
+  }
+
+  sendMessage() {
+    // io.emit()
+  }
+  
+  render() {
+
+    return (
+      <div className="app">
+        <div className="messages-list">
+        
+        </div>
+        <div className="message-input">
+          <input 
+            type="textarea"
+            value={this.state.message}
+
+            className="input"/>
+          <button 
+            className="send"
+            onClick={}>发送</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
